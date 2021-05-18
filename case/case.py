2ebf64530dbb7894036000cf859f7904cef9cbdc
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-05-18 12:20:07 +0300

    added opportunity

--- case/case.py
+++ case/case.py
@@ -10,35 +10,34 @@ def getInsert(table, Id, number, subject, category, owner, status, registered, c
            ', ' + str(responseoverdue) + ', ' + contact + ', ' + account + ', ' + origin + ', ' + respondedon + ', ' + satisfactionlevel + ')'
 
 def writeToFile(data):
-    with open('case_result.csv', 'w', newline='') as csv_file:
-        headers = []
-        for rcrds in data[0]:
-            headers.append(rcrds)
-        datafile = open('result.sql', 'a')
-        for line in data:
-            line['Category'] = getId('CaseCategory', "'" + line['Category'] + "'", 'Name')
-            line['Assignee'] = getId('Contact', "'" + line['Assignee'] + "'", 'Name')
-            line['Status'] = getId('CaseStatus', "'" + line['Status'] + "'", 'Name')
-            line['Priority'] = getId('CasePriority', "'" + line['Priority'] + "'", 'Name')
-            line['Support line'] = getId('SupportLevel', "'" + line['Support line'] + "'", 'Name')
-            line['Service'] = getId('ServiceItem', "'" + line['Service'] + "'", 'Name')
-            line['SLA'] = getId('ServicePact', "'" + line['SLA'] + "'", 'Name')
-            # line['"Overdue resolution"'] = getId('SolutionOverdue', line['"Overdue resolution"'], 'Name')
-            # line['"Overdue response"'] = getId('ResponseOverdue', line['"Overdue response"'], 'Name')
-            line['Contact'] = getId('Contact', "'" + line['Contact'] + "'", 'Name')
-            line['Account'] = getId('Account', "'" + line['Account'] + "'", 'Name')
-            line['Source'] = getId('CaseOrigin', "'" + line['Source'] + "'", 'Name')
-            line['Satisfaction level'] = getId('SatisfactionLevel', "'" + line['Satisfaction level'] + "'", 'Name')
-            insertQuery = getInsert('Case', "'" + line['Id'] + "'", "'" + line['Number'] + "'", "'" + line['Subject'] + "'", line['Category'], line['Assignee'],
-                                    line['Status'], "'" + line['Registration date'] + "'", "'" + line['Created on'] + "'", "'" + line['Response time'] + "'", "'" + line['Actual resolution time'] + "'",
-                                    line['Priority'], line['Support line'], line['Service'], line['SLA'], 1 if line['Overdue resolution'] == 'Yes' else 0, 1 if line['Overdue response'] == 'Yes' else 0,
-                                    line['Contact'], line['Account'], line['Source'], "'" + line['Actual response time'] + "'", line['Satisfaction level'])
-            datafile.write(insertQuery + ";\r\n")
-        datafile.close()
+    headers = []
+    for rcrds in data[0]:
+        headers.append(rcrds)
+    datafile = open('case/result.sql', 'a')
+    for line in data:
+        line['Category'] = getId('CaseCategory', "'" + line['Category'] + "'", 'Name')
+        line['Assignee'] = getId('Contact', "'" + line['Assignee'] + "'", 'Name')
+        line['Status'] = getId('CaseStatus', "'" + line['Status'] + "'", 'Name')
+        line['Priority'] = getId('CasePriority', "'" + line['Priority'] + "'", 'Name')
+        line['Support line'] = getId('SupportLevel', "'" + line['Support line'] + "'", 'Name')
+        line['Service'] = getId('ServiceItem', "'" + line['Service'] + "'", 'Name')
+        line['SLA'] = getId('ServicePact', "'" + line['SLA'] + "'", 'Name')
+        # line['"Overdue resolution"'] = getId('SolutionOverdue', line['"Overdue resolution"'], 'Name')
+        # line['"Overdue response"'] = getId('ResponseOverdue', line['"Overdue response"'], 'Name')
+        line['Contact'] = getId('Contact', "'" + line['Contact'] + "'", 'Name')
+        line['Account'] = getId('Account', "'" + line['Account'] + "'", 'Name')
+        line['Source'] = getId('CaseOrigin', "'" + line['Source'] + "'", 'Name')
+        line['Satisfaction level'] = getId('SatisfactionLevel', "'" + line['Satisfaction level'] + "'", 'Name')
+        insertQuery = getInsert('Case', "'" + line['Id'] + "'", "'" + line['Number'] + "'", "'" + line['Subject'] + "'", line['Category'], line['Assignee'],
+                                line['Status'], "'" + line['Registration date'] + "'", "'" + line['Created on'] + "'", "'" + line['Response time'] + "'", "'" + line['Actual resolution time'] + "'",
+                                line['Priority'], line['Support line'], line['Service'], line['SLA'], 1 if line['Overdue resolution'] == 'Yes' else 0, 1 if line['Overdue response'] == 'Yes' else 0,
+                                line['Contact'], line['Account'], line['Source'], "'" + line['Actual response time'] + "'", line['Satisfaction level'])
+        datafile.write(insertQuery + ";\r\n")
+    datafile.close()
 
 if __name__ == '__main__':
     data = []
-    with open('case.csv', newline='') as File:
+    with open('case/case.csv', newline='') as File:
         reader = csv.DictReader(File)
         for row in reader:
             data.append(row)
